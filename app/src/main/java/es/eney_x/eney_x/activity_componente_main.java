0cbe89f81e945c07c98b7b1a72a220ff4931a1f9
JAIME\jaime <jperezmiceli@gmail.com>
2023-11-16 10:40:04 +0100

    diseño mejorada he incluido nuevos componentes

--- app/src/main/java/es/eney_x/eney_x/activity_componente_main.java
+++ app/src/main/java/es/eney_x/eney_x/activity_componente_main.java
@@ -3,12 +3,36 @@ package es.eney_x.eney_x;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class activity_componente_main extends AppCompatActivity {
-
+    ImageView casa, escudo, ajustes, logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_componente_main);
+
+        cargaImagenes();
+
+    }
+
+    private void cargaImagenes(){
+        casa = (ImageView) findViewById(R.id.botonCasita);
+        casa.setImageResource(R.drawable.casa);
+
+        escudo = (ImageView) findViewById(R.id.boton_escudo);
+        escudo.setImageResource(R.drawable.escudo);
+
+        logo = (ImageView) findViewById(R.id.logo);
+        logo.setImageResource(R.drawable.logoeneyx);
+
+        ajustes = (ImageView) findViewById(R.id.ajustes);
+        ajustes.setImageResource(R.drawable.ajustes);
+/*
+        casa = (ImageView) findViewById(R.id.botonCasita);
+        casa.setImageResource(R.drawable.casa);
+
+
+*/
     }
 }
---
+++ app/src/main/java/es/eney_x/eney_x/activity_perfilUsuario.java
@@ -0,0 +1,6 @@
+package es.eney_x.eney_x;
+
+import android.app.Activity;
+
+public class activity_perfilUsuario extends Activity {
+}
