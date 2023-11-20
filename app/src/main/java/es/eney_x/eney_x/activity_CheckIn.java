8cc98ed053a3830ed32795b20363b1e5b795003c
Juan Felipe Madronero <jmadrbur@myuax.com>
2023-11-20 14:32:18 +0100

    feat: Development of the high quality CheckIn interface, with images, buttons, colors and strings, the constraints remain to be set

---
+++ app/src/main/java/es/eney_x/eney_x/activity_CheckIn.java
@@ -0,0 +1,14 @@
+package es.eney_x.eney_x;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_CheckIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_in);
+    }
+}
