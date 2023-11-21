c9df7ca332f9a098d9665f5feb35189c97871e1e
Anfeh <alexvictoria232@gmail.com>
2023-11-21 14:11:45 +0100

    feat: Changed to another activity

---
+++ app/src/main/java/es/eney_x/eney_x/activity_inicio_eney.java
@@ -0,0 +1,14 @@
+package es.eney_x.eney_x;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_inicio_eney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_eney);
+    }
+}
