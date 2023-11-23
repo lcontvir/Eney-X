38c90cd75dbdcc54c18bd5d9ceb9cb824bf49134
UserJaime <jaimeagueroherguera@gmail.com>
2023-11-23 10:11:20 +0100

    feat: new testing branch

---
+++ app/src/main/java/es/eney_x/eney_x/MainActivity.java
@@ -0,0 +1,13 @@
+package es.eney_x.eney_x;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/es/eney_x/eney_x/activity_perfilUsuario.java
@@ -0,0 +1,11 @@
+package es.eney_x.eney_x;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_perfilUsuario extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_usuario);
+    }
+}
