7457fe4151915fa7ec0b713d8f3a663cfb999a2f
JAIME\jaime <jperezmiceli@gmail.com>
2023-11-23 10:40:19 +0100

    fix: solucionado ya me arranca la aplicacion

--- app/src/main/java/es/eney_x/eney_x/MainActivity.java
+++ app/src/main/java/es/eney_x/eney_x/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_componente_main);
+        setContentView(R.layout.activity_componente_unico);
     }
 }
